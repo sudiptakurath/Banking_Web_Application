82502e3e2c2f35b46aaced1e92cbabf0121a0146
Santhosh Kumar <58925398+santhoshms7@users.noreply.github.com>
2023-07-30 22:47:02 +0530

    Create AccountRepository.java

---
+++ spring/java/repository/AccountRepository.java
@@ -0,0 +1,7 @@
+package com.bank.web.repository;
+
+import com.bank.web.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account, Integer> {
+}
