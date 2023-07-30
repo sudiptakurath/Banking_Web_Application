b713f7caa004a7d0afce709ccfd7679193a9a566
Santhosh Kumar <58925398+santhoshms7@users.noreply.github.com>
2023-07-30 22:45:32 +0530

    Create Account.java

---
+++ spring/java/entity/Account.java
@@ -0,0 +1,127 @@
+package com.bank.web.entity;
+
+import jakarta.persistence.*;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "accounts_tb")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "account_id", updatable = false)
+    private int accountId;
+
+    @Setter
+    @OneToOne(mappedBy = "account")
+    private User user;
+    @Column(name = "fk_user_id")
+    private int userId;
+
+    @Column(name = "account_number")
+    private Long accountNumber;
+    @Column(name = "balance")
+    private Float balance;
+    @Column(name = "account_type")
+    private String accountType;
+    @Column(name = "branch_name")
+    private String branchName;
+    @Column(name = "account_status")
+    private Integer accountStatus;
+    @Column(name = "date_opened")
+    private Date dateOpened;
+
+    @PrePersist
+    void valueGenerator() {
+        int accountNumberLength = 12;
+        long maxAccountNumber = (long) Math.pow(10, accountNumberLength);
+        do {
+            accountNumber = (long) (Math.random() * maxAccountNumber);
+        } while (accountNumber < maxAccountNumber / 10);
+
+        if (balance == null) {
+            balance = 0F;
+        }
+
+        if (accountStatus == null) {
+            accountStatus = 0;
+        }
+
+        if (dateOpened == null) {
+            dateOpened = new Date();
+        }
+    }
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public Long getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(Long accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public Float getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Float balance) {
+        this.balance = balance;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public String getBranchName() {
+        return branchName;
+    }
+
+    public void setBranchName(String branchName) {
+        this.branchName = branchName;
+    }
+
+    public Integer getAccountStatus() {
+        return accountStatus;
+    }
+
+    public void setAccountStatus(Integer accountStatus) {
+        this.accountStatus = accountStatus;
+    }
+
+    public Date getDateOpened() {
+        return dateOpened;
+    }
+
+    public void setDateOpened(Date dateOpened) {
+        this.dateOpened = dateOpened;
+    }
+}
