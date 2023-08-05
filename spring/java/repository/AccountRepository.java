84452a37f3bf8f7e986f1dbf6def756d172c1689
Santhosh Kumar <58925398+santhoshms7@users.noreply.github.com>
2023-08-05 16:31:55 +0530

    Delete spring directory

--- spring/java/repository/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bank.web.repository;
-
-import com.bank.web.entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Integer> {
-}
